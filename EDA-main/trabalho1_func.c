295856c63e46608346c6b20adcae80d4a62c56e7
Liv_Ramos <84927414+livramos@users.noreply.github.com>
2025-09-28 14:56:13 -0300

    trabalho de eda em progresso

--- EDA-main/trabalho1_func.c
+++ EDA-main/trabalho1_func.c
@@ -1,26 +1,40 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 
+#define N 17 
+
 struct paciente{
-    int index;
-    string cor;
-    p* prox;
+    char* cor;
+    struct paciente* prox;  
 }; 
-typedef struct  paciente Paciente;
+typedef struct paciente Paciente;
 
 struct fila{
-    int inicial;
-    int final;
-    float vet[n];
+    Paciente* inicio;
+    Paciente* fim;
 };
 typedef struct fila Fila;
 
 Fila*fila_cria(void){
-    Fila * f=(Fila)malloc(sizeof(Fila));
-    f->n=0;
-    f->ini=0;
+    /*cria lista */
+    Fila * f = (Fila*)malloc(sizeof(Fila));  
+    f->inicio = NULL;     
+    f->fim = NULL;   
     return f;
 }
-
-
-
+void fila_insere(Fila* f, float v ){
+    /*insere a item na lista*/
+    Paciente* n=(Paciente*)malloc(sizeof(Paciente));
+    n->cor= v;
+    n->prox=NULL;
+    if(f->fim!=NULL)
+        f->fim->prox=n;
+    else
+        f->inicio=n;
+        f->fim=n;
+}
+float retira_fila(Fila* f){
+    /*retira um item da fila*/ 
+    Lista*
+}
